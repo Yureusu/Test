2178e4df91b4f24ea89defe278a349825b00aa6f
Yureusu <yureusu9@gmail.com>
2025-01-23 21:15:56 +0800

    editing the form

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+document.getElementById("username").addEventListener("click", username);
+
+function username(){
+    document.getElementById('user-text').style.display = 'block';
+}
+
+document.getElementById("email").addEventListener("click", email);
+
+function email(){
+    document.getElementById('email-text').style.display = 'block';
+}
+
+document.getElementById("password").addEventListener("click", password);
+
+function password(){
+    document.getElementById('password-text').style.display = 'block';
+}
