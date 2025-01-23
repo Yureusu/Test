62870cbda45930cb161e673f807f32efcde80f42
Yureusu <yureusu9@gmail.com>
2025-01-23 20:43:47 +0800

    editing the form

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
 
-document.getElementById('btn1').addEventListener('click', 
-    function(){ alert('Meow!');}
-);
-
-document.getElementById('btn2').addEventListener('click', myFunction);
-
-function myFunction() {
-    alert('Wassup');
-
-    document.getElementById('hello').style.display = 'block';
-}
