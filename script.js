24da9d33a68341ca4736b61d6389e657cff865ec
Yureusu <yureusu9@gmail.com>
2025-01-23 16:07:49 +0800

    added btn display

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ document.getElementById('btn2').addEventListener('click', myFunction);
 
 function myFunction() {
     alert('Wassup');
+
+    document.getElementById('hello').style.display = 'block';
 }
